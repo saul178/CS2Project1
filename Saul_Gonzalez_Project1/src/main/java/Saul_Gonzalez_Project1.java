3fa248e7f78d563c9d4f51795a53e0e4f7ef5860
Saul Gonzalez <gonzalezsaul31@gmail.com>
2023-02-24 00:45:04 -0700

    Finished for now

--- Saul_Gonzalez_Project1/src/main/java/Saul_Gonzalez_Project1.java
+++ Saul_Gonzalez_Project1/src/main/java/Saul_Gonzalez_Project1.java
@@ -1,38 +1,117 @@
+import java.util.Arrays;
+
 public class Saul_Gonzalez_Project1 {
-    private static <T> void testIsEmpty(BagInterface<T> testBag){
-        System.out.println("\nTesting the isEmpty method...");
-        if(testBag.isEmpty()){
-            System.out.println("TEST RESULT: The bag is empty: ");
-        } else {
-            System.out.println("TEST RESULT: The bag is not empty: ");
+    private static final String testString1 = "hello1";
+    private static final String testString2 = "hello2";
+    private static final String testString3 = "hello3";
+
+    public static boolean testGetCurrentSize() {
+        int testSize = 0;
+        BagInterface<String> testCurrentSize = new LinkedBag<>();
+        if (testCurrentSize.isEmpty()) {
+            testCurrentSize.add("J");
+            testSize++;
         }
+
+        return testSize == testCurrentSize.getCurrentSize();
     }
-    private static <T> void testAdd (BagInterface<T> testBag, T[] contents){
-        System.out.println("Testing the add method...");
-        System.out.println("Adding the contents into the LinkedBag...");
-        for (int i = 0; i < contents.length; i++) {
-            if (testBag.add(contents[i])) {
-                System.out.println(contents[i] + " ");
-            } else {
-                System.out.println("Failed to add the content(s) into the bag");
-            }
+
+    public static boolean testIsEmpty() {
+        BagInterface<String> testEmpty = new LinkedBag<>();
+        testEmpty.clear();
+        return testEmpty.isEmpty() && testEmpty.getCurrentSize() == 0;
+    }
+
+    public static boolean testAdd() {
+        BagInterface<String> testAdd = new LinkedBag<>();
+        if (testAdd.isEmpty()) {
+            testAdd.add(testString1);
         }
-        testToArray(testBag);
+
+        boolean areTheyEqual = testAdd.getFrequencyOf(testString1) == testAdd.getCurrentSize();
+
+        return testAdd.getFrequencyOf(testString1) == 1 && areTheyEqual;
     }
-    //test toARRAY
-    private static <T> void testToArray (BagInterface<T> testBag){
-        System.out.println("Testing the toArray method...");
-        System.out.println("The bag contains the following string(s): ");
-        Object[] bagArray = testBag.toArray();
-        for (int i = 0; i < bagArray.length; i++){
-            System.out.print(bagArray[i] + " ");
+
+    public static boolean testRemove() {
+        BagInterface<String> testRemove = new LinkedBag<>();
+        String[] test = {testString2, testString1};
+        if (testRemove.isEmpty()) {
+            testRemove.add(testString1);
+            testRemove.add(testString2);
+            testRemove.add(testString3);
         }
+        testRemove.remove();
+        return Arrays.equals(testRemove.toArray(), test);
     }
-    public static void main(String[] args){
-        BagInterface<String> testBag = new LinkedBag<>();
-        String[] testContentsInBag = {"David", "Kelly", "Luis", "Anthony", "Jason"};
-        testIsEmpty(testBag);
-        testAdd(testBag, testContentsInBag);
-        testIsEmpty(testBag);
+
+    public static boolean testRemoveAnEntry() {
+        BagInterface<String> testRemoveEntry = new LinkedBag<>();
+        if (testRemoveEntry.isEmpty()) {
+            testRemoveEntry.add(testString1);
+            testRemoveEntry.add(testString2);
+            testRemoveEntry.add(testString3);
+        }
+
+        testRemoveEntry.remove(testString1);
+
+        return !testRemoveEntry.contains(testString1);
+    }
+
+    public static boolean testClear() {
+        BagInterface<String> testClear = new LinkedBag<>();
+        if (testClear.isEmpty()) {
+            testClear.add(testString1);
+            testClear.add(testString2);
+            testClear.add(testString3);
+        }
+        if (!testClear.isEmpty()) {
+            testClear.clear();
+        }
+        return testClear.isEmpty();
+    }
+
+    public static boolean testGetFrequency() {
+        LinkedBag<String> testFrequency = new LinkedBag<>();
+
+        if (testFrequency.isEmpty()) {
+            testFrequency.add(testString1);
+            testFrequency.add(testString2);
+            testFrequency.add(testString2);
+        }
+
+        return testFrequency.getFrequencyOf(testString1) == 1 && testFrequency.getFrequencyOf(testString2) == 2;
+    }
+
+    public static boolean testContains() {
+        LinkedBag<String> containsTest = new LinkedBag<>();
+        if (containsTest.isEmpty()) {
+            containsTest.add(testString1);
+            containsTest.add(testString2);
+        }
+        return containsTest.contains(testString1) && containsTest.getFrequencyOf(testString1) == 1;
+    }
+
+    public static boolean testToArray() {
+        LinkedBag<String> arrayTest = new LinkedBag<>();
+        String[] testArrayManually = {testString1, testString2, testString3};
+
+        arrayTest.add(testString3);
+        arrayTest.add(testString2);
+        arrayTest.add(testString1);
+
+        return Arrays.equals(arrayTest.toArray(), testArrayManually);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("TEST Empty: " + testIsEmpty());
+        System.out.println("TEST CurrentSize: " + testGetCurrentSize());
+        System.out.println("TEST Add: " + testAdd());
+        System.out.println("TEST Remove: " + testRemove());
+        System.out.println("TEST RemoveAnEntry: " + testRemoveAnEntry());
+        System.out.println("TEST Clear: " + testClear());
+        System.out.println("TEST GetFrequency: " + testGetFrequency());
+        System.out.println("TEST Contains: " + testContains());
+        System.out.println("TEST ToArray: " + testToArray());
     }
 }
